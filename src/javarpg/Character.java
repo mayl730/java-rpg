1e523ba54ddcd0f70816ad841b548df8f408aa63
mayl730 <maylaixyz@gmail.com>
2023-12-01 11:34:56 +0900

    add: hp

--- src/javarpg/Character.java
+++ src/javarpg/Character.java
@@ -1,5 +1,13 @@
 package javarpg;
 
 public abstract class Character {
+	    protected int hp;
 
+	    public Character(int hp) {
+	        this.hp = hp;
+	    }
+
+	    public int getHp() {
+	        return hp;
+	    }
 }
