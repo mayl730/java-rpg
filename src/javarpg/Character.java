9fbc10d01a34acf4b44771d2b19652018de5b3b8
mayl730 <maylaixyz@gmail.com>
2023-12-01 11:42:02 +0900

    add: implement character class

--- src/javarpg/Character.java
+++ src/javarpg/Character.java
@@ -1,13 +1,20 @@
 package javarpg;
 
 public abstract class Character {
-	    protected int hp;
-
-	    public Character(int hp) {
-	        this.hp = hp;
+		protected String name;
+		protected int hp;
+	    
+	    public String getName() {
+	        return name;
 	    }
-
 	    public int getHp() {
 	        return hp;
 	    }
+	    
+	    public void setName(String name) {
+	        this.name = name;
+	    }
+	    public void setHp(int hp) {
+	        this.hp = hp;
+	    }    
 }
